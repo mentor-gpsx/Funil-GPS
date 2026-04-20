e31c2c5083672f4cf408ad3e379ecf18dba359f5
GPS.X <venda@gpsx.com.br>
2026-04-20 13:41:41 -0300

    fix: reduce clickup limit to 200 to avoid timeout

--- api/clickup.js
+++ api/clickup.js
@@ -23,7 +23,7 @@ async function fetchClickUpList(listId) {
     const options = {
       hostname: 'api.clickup.com',
       port: 443,
-      path: `/api/v2/list/${listId}/task?include_subtasks=false&limit=500`,
+      path: `/api/v2/list/${listId}/task?include_subtasks=false&limit=200`,
       method: 'GET',
       headers: {
         'Authorization': CLICKUP_API_KEY,
